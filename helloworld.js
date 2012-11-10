b88a06da41e511dd97fd9576566d81f2d79984cf
tamurashingo <tamura.shingo@gmail.com>
2012-11-10 23:36:49 +0900

    #1 fixed.

--- helloworld.js
+++ helloworld.js
@@ -5,26 +5,23 @@
 ( function( $ ) {
 
     $.fn.helloworld = function( options ) {
-        var $this = this;
         var config = {
             width : this.width(),
             height : this.height(),
             message : "hello world"
         };
 
-        var initialize = function() {
+        return this.each( function() {
             if ( options ) {
-                $.extend( config, options );
+        	    $.extend( config, options );
             }
-            $this.addClass('jquery-helloworld').width(config.width).height(config.height);
+            $this = $(this);
+            $this.addClass("jquery-helloworld").width(config.width).height(config.height);
             var msg = $('<div class="jquery-helloworld-message">' + config.message + '</div>');
             msg.bind( 'click', function() { alert( config.message ); } );
-            $this.add( msg );
-        };
-
-        initialize();
+            $this.after( msg );
+        });
     };
-
 
 } )( jQuery );
 
