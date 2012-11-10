6db82971737a61a768c9333cebe3d1426f85d2e8
tamurashingo <tamura.shingo@gmail.com>
2012-11-10 21:40:00 +0900

    tab to space

--- helloworld.js
+++ helloworld.js
@@ -5,24 +5,24 @@
 ( function( $ ) {
 
     $.fn.helloworld = function( options ) {
-	var $this = this;
-	var config = {
-	    width : this.width(),
-	    height : this.height(),
-	    message : "hello world"
-	};
+        var $this = this;
+        var config = {
+            width : this.width(),
+            height : this.height(),
+            message : "hello world"
+        };
 
-	var initialize = function() {
-	    if ( options ) {
-		$.extend( config, options );
-	    }
-	    $this.addClass('jquery-helloworld').width(config.width).height(config.height);
-	    var msg = $('<div class="jquery-helloworld-message">' + config.message + '</div>');
-	    msg.bind( 'click', function() { alert( config.message ); } );
-	    $this.add( msg );
-	};
+        var initialize = function() {
+            if ( options ) {
+                $.extend( config, options );
+            }
+            $this.addClass('jquery-helloworld').width(config.width).height(config.height);
+            var msg = $('<div class="jquery-helloworld-message">' + config.message + '</div>');
+            msg.bind( 'click', function() { alert( config.message ); } );
+            $this.add( msg );
+        };
 
-	initialize();
+        initialize();
     };
 
 
